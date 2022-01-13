40ffc0c7667dc570700612f99a62945605255306
Guilherme longo <longo.guilherme@hotmail.com>
2022-01-13 19:46:04 -0300

    Primeiro teste

---
+++ IntroOrientadaAObjetos/IntroOrientadaAObjetos/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroOrientadaAObjetos
+{
+   public class Pessoa
+    {
+
+        public int idade;
+        public string nome;
+
+    }
+}
---
+++ IntroOrientadaAObjetos/IntroOrientadaAObjetos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IntroOrientadaAObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa a, b;
+
+           a = new Pessoa();
+            b= new Pessoa();
+
+            Console.WriteLine("Digite idade e o nome:");
+            a.idade = int.Parse(Console.ReadLine());
+            a.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite idade e o nome:");
+            b.idade = int.Parse(Console.ReadLine());
+            b.nome = Console.ReadLine();
+
+            if (a.idade < b.idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + b.nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + a.nome);
+            
+            }
+        }
+    }
+}
